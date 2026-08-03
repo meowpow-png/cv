825d9d9b89404adc32e02483e5e21f4e07930638
meowpow-png <marin.softic@proton.me>
2026-08-03 14:52:02 +0200

    Standardize page size to A4

--- src/print.ts
+++ src/print.ts
@@ -5,7 +5,13 @@ async function main() {
   const page = await browser.newPage()
 
   await page.goto(`file://${process.cwd()}/dist/cv.html`)
-  await page.pdf({ path: 'dist/cv.pdf', printBackground: true })
+  await page.pdf({
+    path: 'dist/cv.pdf',
+    printBackground: true,
+    width: '210mm',
+    height: '297mm',
+    margin: { top: 0, right: 0, bottom: 0, left: 0 },
+  })
 
   await browser.close()
 }
