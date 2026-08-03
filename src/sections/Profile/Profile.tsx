310955de1b3a1f3c91ade9c5d8c4e4df302c210c
meowpow-png <marin.softic@proton.me>
2026-08-03 17:03:05 +0200

    Remove profile section heading

--- src/sections/Profile/Profile.tsx
+++ src/sections/Profile/Profile.tsx
@@ -1,10 +1,8 @@
 import { profile as ProfileContent } from '@/content/default'
-import { SectionHeading } from '@/shared/components/SectionHeading'
 
 export function Profile() {
   return (
     <section className="profile">
-      <SectionHeading name="profile" />
       <ProfileContent />
     </section>
   )
