c478c41821a4f29afc6b836bec4338ace8695092
meowpow-png <marin.softic@proton.me>
2026-08-31 16:24:22 +0200

    Clear generic CV type content

--- src/apps/generic/App.tsx
+++ src/apps/generic/App.tsx
@@ -1,25 +1,5 @@
-import { Education } from '@/sections/Education'
-import { Footer } from '@/sections/Footer'
-import { Hero } from '@/sections/Hero'
-import { Profile } from '@/sections/Profile'
-import { Projects } from '@/sections/Projects'
-import { Skills } from '@/sections/Skills'
-
-import { education, footer, hero, profile, projects, skills } from '@/content/default'
-
 export function App() {
-  return (
-    <>
-      <div className="app-content">
-        <Hero content={hero} />
-        <Profile content={profile} />
-        <Skills content={skills} />
-        <Projects content={projects} />
-        <Education content={education} />
-        <Footer content={footer} />
-      </div>
-    </>
-  )
+  return <div className="app-content" />
 }
 
 export const cssPath = 'src/apps/generic/App.css'
