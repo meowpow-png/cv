cf9fdb3f60092ea4dcdd7cbc81208f78fc99f7c1
meowpow-png <marin.softic@proton.me>
2026-08-31 20:28:35 +0200

    Write generic CV section mdx content

--- src/content/generic/index.ts
+++ src/content/generic/index.ts
@@ -1,8 +1,30 @@
 import { readMdx } from '@/shared/mdx'
 
-import { type HeroContent, type ProfileContent } from '@/shared/content-types'
+import { type ComponentType } from 'react'
+import {
+  type EducationContent,
+  type HeroContent,
+  type ProfileContent,
+  type WorkExperienceEntry,
+  type WorkExperienceFrontmatter,
+} from '@/shared/content-types'
 
 export const profile = readMdx('src/content/generic/profile.mdx').default as ProfileContent
+
+const workExperienceSlugs = ['konobar', 'zavarivac']
+
+export const workExperience: WorkExperienceEntry[] = workExperienceSlugs.map((slug) => {
+  const entry = readMdx(`src/content/generic/work-experience/${slug}.mdx`)
+
+  return {
+    frontmatter: entry.frontmatter as WorkExperienceFrontmatter,
+    Description: entry.default,
+  }
+})
+
+export const education = readMdx('src/content/generic/education.mdx')
+  .frontmatter as EducationContent
+export const languages = readMdx('src/content/generic/languages.mdx').default as ComponentType
 
 const heroFrontmatter = readMdx('src/content/generic/hero.mdx').frontmatter as HeroContent
 
